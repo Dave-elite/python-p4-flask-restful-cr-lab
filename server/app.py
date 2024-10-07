165d2f2df913097f477de16c04ddcef4316725e3
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-10-07 08:09:12 +0300

    lab complete

--- server/app.py
+++ server/app.py
@@ -17,10 +17,54 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    
+    def get(self):
+        plants_dict_list =[pl.to_dict() for pl in Plant.query.all()]
+        if plants_dict_list is None:
+            return {"Message": "No plant found"}
+        response = make_response(
+            plants_dict_list,
+            200,
+        )
+        return response
+
+    
+    def post(self):
+        new_plant = Plant(
+            name=request.json['name'],
+            image=request.json['image'],
+            price=request.json['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        new_plant_dict = new_plant.to_dict()
+        
+        response = make_response(
+            new_plant_dict,
+            201,
+        )
+        return response
+        
+    
+    
+api.add_resource(Plants, '/plants')
+        
+
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id ).first().to_dict()
+        
+        response = make_response(
+            plant,
+            200,
+        )
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
